2e3d539ee8085d6dc68a0d2e1f42bcf6020c00ff
hemorexal-eu <76467539+hemorexal-eu@users.noreply.github.com>
2020-12-23 14:54:28 +0200

    1

---
+++ js/translater.js
@@ -0,0 +1,227 @@
+var new_lang = {
+
+"lt0": "GEL\"Hemorexual\"",
+
+"lt1": "<span>TRATAMENTUL EFICIENT AL HEMOROIZILOR</span> ÎN ORICE ETAPĂ",
+
+"lt2": "DUREREA ȘI MÂNCĂRIMEA TREC<br>ÎN<span>5-10 MINUTE!</span>",
+
+"lt3": "Scapă de durerea, mâncărimea și arderea",
+
+"lt4": "Promovează resorbția hemoroizilor",
+
+"lt5": "Previne dezvoltarea repetată a bolii",
+
+"lt6": "Prețul vechi: <br><span class=\"price_old\"><span class=\"price_main_value\">98</span><span class=\"price_main_currency\">EUR</span></span>",
+
+"lt7": "Preț nou: <br><span class=\"price_main\"><span class=\"price_main_value\">49</span><span class=\"price_main_currency\">EUR</span></span>",
+
+"lt8": "PÎNĂ LA SFÎRȘITUL ACȚIUNII A RĂMAS:",
+
+"lt24": "COMANDĂ ACUM*",
+
+"lt25": "*Confidențialitatea este garantată",
+
+"lt26": "SCAPĂ<br>DE BOALA<br><span>O DATĂ PENTRU TOTDEAUNA</span>",
+
+"lt27": "Hemoroizii aduc senzațiile cele mai neplăcute și foarte dureroase, împiedică să conducă un stil de viață obișnuit. Fără tratament, boala progresează întotdeauna, cursul său se agravează rapid, până la incapacitatea de a repoziționa nodurile căzute.",
+
+"lt28": "Se dovedește că hemoroizii pot duce la cancer rectal în 1-3 ani după apariția primelor simptome.",
+
+"lt29": "Nu așteptați ca durerea să devină insuportabilă, vor apărea fisuri anale greu de vindecat, va începe o cădere constantă a nodurilor. Luați măsuri tocmai acum!",
+
+"lt30": "<span>Hemorexual</span> - o acțiune îndreptată împotriva hemoroizilor",
+
+"lt31.": "Îndepărtează imediat durerea, mâncărimea, senzația de strângere",
+
+"lt32.": "Oprește sângerarea fără riscul de tromboză",
+
+"lt33": "Normalizează tonul și întărește pereții venoși",
+
+"lt34": "Reduce umflarea și inflamația, elimină fenomenele stagnante",
+
+"lt35": "Vindecă chiar și crăpăturile interioare adânci",
+
+"lt36": "Accelerează în mod repetat regenerarea țesuturilor deteriorate",
+
+"lt37": "Ajută la reducerea hemoroizilor",
+
+"lt38": "REVINE RAPID LA O STARE SĂNĂTOASĂ",
+
+"lt39": "<span>ALTERNATIVĂ SĂNĂTOASĂ</span>CU TRATAMENTE RADICALE",
+
+"lt40": "Amintiți-vă cât de minunat a fost să trăiți fără hemoroizi? Veți reveni acest timp fără griji imediat ce începeți să aplicați gelul \"Hemorexual\".",
+
+"lt41": "Fără cheltuieli impresionante pentru tratament la proctolog, fără medicamente scumpe și intervenții chirurgicale traumatice.",
+
+"lt42": "Igienic și convenabil de utilizat",
+
+"lt43": "Alinarea după prima aplicare",
+
+"lt44": "Spune Nu situațiilor stânjenitoare și rușinarei!",
+
+"lt45": "100% <span>COMPOZIȚIE</span> NATURALĂ",
+
+"lt46": "Hemorexual conține componentele naturale care elimină ușor disconfortul și alte simptome asociate, și au un efect terapeutic asupra cauzei problemei",
+
+"lt47": "Usturoi",
+
+"lt48": "Analgezic pronunțat, acțiune antibacteriană și antitumorală.",
+
+"lt49": "Rostopască",
+
+"lt50": "Conține alcaloizi de izochinolină care au un efect antiinflamator. În mod eficient alinează mâncărimea pielii.",
+
+"lt51": "Extract de Saw Palmetto",
+
+"lt52": "Ameliorează umflarea și durerea, are un puternic efect venotonizant și participă la resorbția umflărilor externe și interne.",
+
+"lt53": "Extract de fructe de palmier sabal",
+
+"lt54": "Ajută la eliminarea inflamației, durerii și mâncărimei. Elimină senzația de \"corp străin\" și arderea în anus. Vindecă fisurile.",
+
+"lt55": "Extract de coada șoricelului",
+
+"lt56": "Îmbunătățește circulația sângelui în vase, ameliorează inflamația, crește fluxul sanguin, ajută la eliminarea cheagurilor de sânge formate.",
+
+"lt57": "Scoarţa stejarului",
+
+"lt58": "Oprește sângele, elimină umflarea, întărește pereții vaselor de sânge. Creează o barieră naturală pentru microflora patogenă.",
+
+"lt59": "Aloe",
+
+"lt60": "Are un efect antiinflamator, ameliorează starea de exacerbare.",
+
+"lt61": "Mușețel farmaceutic",
+
+"lt62": "Crește viteza de regenerare a țesuturilor, are un efect analgezic.",
+
+"lt63": "Cartofi",
+
+"lt64": "Amidonul din compoziția sa are proprietăți de învelire și antiinflamare.",
+
+"lt65": "Usturoi",
+
+"lt66": "Analgezic pronunțat, acțiune antibacteriană și antitumorală.",
+
+"lt67": "Rostopască",
+
+"lt68": "Conține alcaloizi de izochinolină care au un efect antiinflamator. În mod eficient alinează mâncărimea pielii.",
+
+"lt69": "Extract de Saw Palmetto",
+
+"lt70": "Ameliorează umflarea și durerea, are un puternic efect venotonizant și participă la resorbția umflărilor externe și interne.",
+
+"lt71": "Extract de fructe de palmier sabal",
+
+"lt72": "Ajută la eliminarea inflamației, durerii și mâncărimei. Elimină senzația de \"corp străin\" și arderea în anus. Vindecă fisurile.",
+
+"lt73": "Extract de coada șoricelului",
+
+"lt74": "Îmbunătățește circulația sângelui în vase, ameliorează inflamația, crește fluxul sanguin, ajută la eliminarea cheagurilor de sânge formate.",
+
+"lt75": "Scoarţa stejarului",
+
+"lt76": "Oprește sângele, elimină umflarea, întărește pereții vaselor de sânge. Creează o barieră naturală pentru microflora patogenă.",
+
+"lt77": "Aloe",
+
+"lt78": "Are un efect antiinflamator, ameliorează starea de exacerbare.",
+
+"lt79": "Mușețel farmaceutic",
+
+"lt80": "Crește viteza de regenerare a țesuturilor, are un efect analgezic.",
+
+"lt81": "Cartofi",
+
+"lt82": "Amidonul din compoziția sa are proprietăți de învelire și antiinflamare.",
+
+"lt83": "Usturoi",
+
+"lt84": "Analgezic pronunțat, acțiune antibacteriană și antitumorală.",
+
+"lt85": "Rostopască",
+
+"lt86": "Conține alcaloizi de izochinolină care au un efect antiinflamator. În mod eficient alinează mâncărimea pielii.",
+
+"lt87": "OPINIA EXPERTULUI",
+
+"lt88": "Remediile moderne pentru tratamentul hemoroizilor sunt puternic inferioare medicamentelor pe bază naturală în ceea ce privește gradul de expunere la cauza principală a bolii. Substanțele chimice reduc absorbția substanței active și provoacă o serie de efecte secundare: mâncărime crescută, umflături, reacții alergice.",
+
+"lt89": "În acest sens,\"Hemorexual\" s-a dovedit a fi cel mai eficient medicament în tratamentul hemoroizilor în toate etapele: în compoziția sa, există numai componente naturale cu un grad de absorbție de 99,7%. Gelul nu are efecte secundare, nu provoacă reacții alergice și poate fi folosit ca un agent expres la îndemână, deoarece nu lasă urme pe haine și ameliorează sursa durerii timp de 5-10 minute.",
+
+"lt90": "Ca un medic cu 20 de ani de experiență în proctologie, recomand tratamentul hemoroizilor cu Hemorexual în orice stadiu al bolii.",
+
+"lt91": "<span>Mihai Spânu,</span><br> este un proctologist cu experiență de 20 de ani, doctor în științe medicale",
+
+"lt92": "AVANTAJELE ALE \"Hemorexual\"",
+
+"lt93": "Utilizare convenabilă",
+
+"lt94": "Se absoarbe imediat după aplicare, fără a lăsa lipici și urme pe haine",
+
+"lt95": "Compoziție naturală",
+
+"lt96": "Doar ingredientele naturale, fără substanțe chimice periculoase și hormoni",
+
+"lt97": "Siguranță",
+
+"lt98": "Nu produce efecte secundare, poate fi utilizat pentru o lungă perioadă de timp",
+
+"lt99": "Rezultatul prelungit",
+
+"lt100": "Doar 1 curs de aplicare vă permite să eliminați toate manifestările hemoroizilor*",
+
+"lt101": "*durata administrării se calculează individual",
+
+"lt102": "Versatilitate",
+
+"lt103": "Potrivit pentru utilizare atât în exterior, cât și în interior, în rect",
+
+"lt104": "RECENZII ALE PACIENȚILOR",
+
+"lt105": "Petru,<span>44 de ani:</span>",
+
+"lt106": "Am comandat \"Hemorexual\" pe acest site. Nu am observat efecte secundare. În 5 zile, hemoroizii mi-au trecut complet, nodul a dispărut, durerea a dispărut atunci când mergeam la toaletă. Regret că nu am început imediat să mă tratez anume cu \"Hemorexual\".",
+
+"lt107": "Alex, <span>36 de ani:</span>",
+
+"lt108": "Cel mai mare nod a redus în doar o lună de utilizare \"Hemorexual\" de două ori pe zi. Nu mai am probleme cu toaleta, nu mă deranjează disconfortul, pot sta mult timp, ceea ce este important pentru mine, pentru că petrec multe ore la volan.",
+
+"lt109": "Cristina,<span>33 de ani:</span>",
+
+"lt110": "La un an după naștere, a apărut o astfel de problemă delicată - hemoroizi. Totul mă durea tare, nici măcar nu puteam să mă ocup de copilul pe deplin. \"Hemorexual\" a ajutat bine, fisura a fost strânsă, două umflături care au fost, nu se bâlbâie.",
+
+"lt111": "GEL \"Hemorexual\"",
+
+"lt112": "PUNEȚI CAPĂT SUFERINȚEI DVS.!",
+
+"lt113": "Hemorexual vă va<br>scuti de hemoroizi pentru 1 curs",
+
+"lt114": "Tratamentul eficient al hemoroizilor",
+
+"lt115": "Alinarea rapidă a durerii și eliminarea altor simptome",
+
+"lt116": "Rezultatul persistent după 1 curs",
+
+"lt117": "Prețul vechi: <br><span class=\"price_old\"><span class=\"price_main_value\">98</span><span class=\"price_main_currency\">EUR</span></span>",
+
+"lt118": "Preț nou: <br><span class=\"price_main\"><span class=\"price_main_value\">49</span><span class=\"price_main_currency\">EUR</span></span>",
+
+"lt119": "PÎNĂ LA SFÎRȘITUL ACȚIUNII A RĂMAS:",
+
+"lt135": "COMANDĂ ACUM*",
+
+"lt136": "*Confidențialitatea este garantată",
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
